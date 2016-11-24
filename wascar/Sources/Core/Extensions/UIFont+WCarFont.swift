798d63a947372f595d693e423107a03c0f4300a7
Wagner Sales <salesawagner@gmail.com>
2016-11-24 14:03:07 +1100

    Add font methods for app font default

--- wascar/Sources/Core/Extensions/UIFont+WCarFont.swift
+++ wascar/Sources/Core/Extensions/UIFont+WCarFont.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 extension UIFont {
+	
+	static func WCARFontWithSize(fontSize: CGFloat = 14) -> UIFont {
+		return UIFont(name: "Avenir-Book", size: fontSize) ?? UIFont.systemFontOfSize(fontSize)
+	}
+	
 	static func WCARnavigationBarFont() -> UIFont {
 		let fontSize: CGFloat = 17
 		return UIFont(name: "Avenir-Medium", size: fontSize) ?? UIFont.systemFontOfSize(fontSize)
